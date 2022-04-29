853b3240437af4f9a2116976f4bf6a2ac8d8a1ee
fransihsan <fransihsan8888@gmail.com>
2022-04-29 14:21:31 +0700

    add respon get all articles

--- deliveries/controllers/article/response.go
+++ deliveries/controllers/article/response.go
@@ -22,3 +22,23 @@ func ToResponseCreateArticle(Article A.Articles) ResponseCreateArticle {
 		CreatedAt: Article.CreatedAt,
 	}
 }
+
+type ResponseGetAllArticles struct {
+	ID        uint      `json:"id"`
+	Author    string    `json:"author"`
+	Title     string    `json:"title"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func ToResponseGetAllArticles(Articles []A.Articles) []ResponseGetAllArticles {
+	responses := make([]ResponseGetAllArticles, len(Articles))
+	for i := 0; i < len(Articles); i++ {
+		responses[i].ID = Articles[i].ID
+		responses[i].Author = Articles[i].Author
+		responses[i].Title = Articles[i].Title
+		responses[i].Body = Articles[i].Body
+		responses[i].CreatedAt = Articles[i].CreatedAt
+	}
+	return responses
+}
