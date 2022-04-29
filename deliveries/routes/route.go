57afcad6d20c76a685120fb001311572c0e4129f
fransihsan <fransihsan8888@gmail.com>
2022-04-29 18:02:00 +0700

    add route endpoint

--- deliveries/routes/route.go
+++ deliveries/routes/route.go
@@ -1,1 +1,19 @@
 package routes
+
+import (
+	"articles-api/deliveries/controllers/article"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func RegisterPaths(e *echo.Echo, ac *article.ArticleController) {
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+
+	// User Route
+	e.POST("/articles", ac.Create())
+	e.GET("/articles", ac.GetAllArticles())
+}
