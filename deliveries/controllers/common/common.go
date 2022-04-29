e4f2c328f88b6b61a3349b299b41f353dbe97403
fransihsan <fransihsan8888@gmail.com>
2022-04-29 13:44:15 +0700

    add respon internal server error

--- deliveries/controllers/common/common.go
+++ deliveries/controllers/common/common.go
@@ -1,1 +1,19 @@
 package common
+
+import "net/http"
+
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func InternalServerError(message string) Response {
+	return Response{
+		Code:    http.StatusInternalServerError,
+		Message: message,
+		Data:    nil,
+	}
+}
+
+
