441f9d645f36225a0c9429168149e229fba24caa
fransihsan <fransihsan8888@gmail.com>
2022-05-01 00:40:54 +0700

    add mock article repository

--- repositories/mock/article/article.go
+++ repositories/mock/article/article.go
@@ -1,2 +1,14 @@
 package article
 
+import (
+	A "articles-api/entities/article"
+)
+
+func MockArticle() A.Articles {
+	mockArticle := A.Articles{
+		Author: "frans",
+		Title:  "title",
+		Body:   "body",
+	}
+	return mockArticle
+}
