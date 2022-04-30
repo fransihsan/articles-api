fb9b6931c4e0941d1bd75448bff23f1042d908b8
fransihsan <fransihsan8888@gmail.com>
2022-05-01 00:56:42 +0700

    add unit test get article repository

--- repositories/article/article_test.go
+++ repositories/article/article_test.go
@@ -39,3 +39,45 @@ func TestCreate(t *testing.T) {
 		assert.NotNil(t, err)
 	})
 }
+
+func TestGetAllArticles(t *testing.T) {
+	Migrator()
+	repo := NewArticleRepository(db)
+
+	mockArticle := ArticleMock.MockArticle()
+
+	t.Run("negative /articles", func(t *testing.T) {
+		_, err := repo.GetAllArticles("","")
+		assert.NotNil(t, err)
+	})
+
+	t.Run("negative /articles?author", func(t *testing.T) {
+		_, err := repo.GetAllArticles("frans","")
+		assert.NotNil(t, err)
+	})
+
+	t.Run("negative /articles?query", func(t *testing.T) {
+		_, err := repo.GetAllArticles("","body")
+		assert.NotNil(t, err)
+	})
+
+	t.Run("positive /articles", func(t *testing.T) {
+		repo.Create(mockArticle)
+
+		res, err := repo.GetAllArticles("","")
+		assert.Nil(t, err)
+		assert.Equal(t, mockArticle.Author, res[0].Author)
+	})
+
+	t.Run("positive /articles?author", func(t *testing.T) {
+		res, err := repo.GetAllArticles("frans","")
+		assert.Nil(t, err)
+		assert.Equal(t, mockArticle.Author, res[0].Author)
+	})
+
+	t.Run("positive /articles?query", func(t *testing.T) {
+		res, err := repo.GetAllArticles("","body")
+		assert.Nil(t, err)
+		assert.Equal(t, mockArticle.Author, res[0].Author)
+	})
+}
