b9bacccee0eaf289e499ed6f87594b413611edac
fransihsan <fransihsan8888@gmail.com>
2022-05-01 01:42:14 +0700

    add mock create and getallarticles controller

--- deliveries/mocks/article/article.go
+++ deliveries/mocks/article/article.go
@@ -1,2 +1,42 @@
 package article
 
+import (
+	A "articles-api/entities/article"
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type MockArticleRepository struct{}
+
+func (repo *MockArticleRepository) Create(newArticle A.Articles) (A.Articles, error) {
+	return A.Articles{Model: gorm.Model{ID: 1}, Author: "frans", Title: "title", Body: "body"}, nil
+}
+
+func (repo *MockArticleRepository) GetAllArticles() ([]A.Articles, error) {
+	article1 := A.Articles{
+		Model:  gorm.Model{ID: 1},
+		Author: "frans",
+		Title:  "title",
+		Body:   "body",
+	}
+
+	article2 := A.Articles{
+		Model:  gorm.Model{ID: 2},
+		Author: "ihsan",
+		Title:  "title2",
+		Body:   "body2",
+	}
+
+	return []A.Articles{article2, article1}, nil
+}
+
+type MockFalseArticleRepository struct{}
+
+func (repo *MockFalseArticleRepository) Create(newArticle A.Articles) (A.Articles, error) {
+	return A.Articles{}, errors.New("gagal membuat artikel baru")
+}
+
+func (repo *MockFalseArticleRepository) GetAllArticles() ([]A.Articles, error) {
+	return nil, errors.New("tidak terdapat data artikel sama sekali")
+}
