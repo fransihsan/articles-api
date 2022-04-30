f41b8501d06e01c4ab9f5a6113f24e6387900498
fransihsan <fransihsan8888@gmail.com>
2022-05-01 00:41:42 +0700

    add unit test create article repository

--- repositories/article/article_test.go
+++ repositories/article/article_test.go
@@ -1,2 +1,41 @@
 package article
 
+import (
+	"articles-api/configs"
+	A "articles-api/entities/article"
+	ArticleMock "articles-api/repositories/mock/article"
+	"articles-api/utils"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	config = configs.GetConfig(true)
+	db     = utils.InitDB(config)
+)
+
+func Migrator() {
+	db.Migrator().DropTable(&A.Articles{})
+
+	db.AutoMigrate(&A.Articles{})
+}
+
+func TestCreate(t *testing.T) {
+	Migrator()
+	repo := NewArticleRepository(db)
+
+	mockArticle := ArticleMock.MockArticle()
+
+	t.Run("positive", func(t *testing.T) {
+		res, err := repo.Create(mockArticle)
+		assert.Nil(t, err)
+		assert.Equal(t, mockArticle.Author, res.Author)
+	})
+
+	t.Run("negative", func(t *testing.T) {
+		mockArticle.ID = 1
+		_, err := repo.Create(mockArticle)
+		assert.NotNil(t, err)
+	})
+}
